86f1183e799ed2e45dd9923e9fd1cef9bb50abe0
Eric Boisvenu <burn4live@gmail.com>
2019-11-04 11:36:17 -0700

    added return link

--- plugins/axios.js
+++ plugins/axios.js
@@ -13,6 +13,7 @@ export default function ({ $axios, redirect }) {
   $axios.onResponseError((error) => {
     const code = parseInt(error.response && error.response.status)
     if (code === 401) {
+      window.location.replace('https://dev-2upadx1s.auth0.com/v2/logout?returnTo=http%3A%2F%2Flocalhost:3333/')
       redirect('/login')
     }
   })
