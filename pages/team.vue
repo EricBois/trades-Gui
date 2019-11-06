62870fe825bdec3a5593d0d82dd22fea4fbeb639
Eric Boisvenu <burn4live@gmail.com>
2019-11-06 13:00:13 -0700

    added team

--- pages/team.vue
+++ pages/team.vue
@@ -1,3 +1,32 @@
 <template>
-  <v-flex />
+  <v-container>
+    <v-row>
+      <v-col xs12 sm4 cols="3">
+        <v-card>
+          <v-card v-for="user in users" :key="user.id" pl-2>{{user.name}} <v-divider/> </v-card>
+        </v-card>
+      </v-col>
+      <v-col xs12 sm6>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      team: [],
+      users: []
+    }
+  },
+  created () {
+    this.$axios.$get('team/fetch').then((res) => {
+      this.team = res
+    })
+    this.$axios
+      .$get('account/public').then((res) => {
+        this.users = res
+      })
+  }
+}
+</script>
