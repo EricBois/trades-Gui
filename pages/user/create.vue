3f891a7d70483f3d03d245244654ee41f03869e5
Eric Boisvenu <burn4live@gmail.com>
2019-11-24 13:58:29 -0700

    Added store profile

--- pages/user/create.vue
+++ pages/user/create.vue
@@ -218,17 +218,11 @@ export default {
       } else {
         this.info.liability = false
       }
-    },
-    profile () {
-      if (this.profile.user_metadata && this.profile.user_metadata.phone) {
-        this.info.phone = this.profile.user_metadata.phone
-      }
-      this.info.email = this.$auth.user.email
     }
   },
   created () {
-    if (this.profile.user_metadata && this.profile.user_metadata.phone) {
-      this.info.phone = this.profile.user_metadata.phone
+    if (this.$auth.user['https://subhub.com/user_metadata'].phone) {
+      this.info.phone = this.$auth.user['https://subhub.com/user_metadata'].phone
     }
     this.info.email = this.$auth.user.email
   },
