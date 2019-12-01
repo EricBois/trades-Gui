f75383b32dd35bfb9409223ebe72b04f075ae1b4
Eric Boisvenu <burn4live@gmail.com>
2019-11-30 17:32:18 -0700

    added tips message

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
     trades: "Drywall, Taping, Framing, Labour, Texturing, Insulation",
     tickets: "WHIMIS, First Aid, Scissor Lift, Fall Arrest",
     welcome: "Few things to get started, Let's start with building your profile!",
-    welcomeProfile: "Make sure to fill in your name and add as much information as possible for the prospect clients / bidders to see",
+    welcomeProfile: "Make sure to fill in your name and add as much information as possible for the prospect clients / bidders to see. If you'd like to make some change at a later date you can always access this page from the left menu on the top ( your name )",
     welcomeJob: "Here you can place a bid, direct message or lookup the contractor and get all available details about the project ( photos, files ) ...",
     welcomeJobs: "Here's all the available public, private and your own posted project listings awaiting bidding, click on them for more info and to place a bid.",
     privateJobs: "The projects in this section are invite to bid on private projects or your own private posting.",
