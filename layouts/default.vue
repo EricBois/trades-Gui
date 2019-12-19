4182c2ca0ef53f358080e880e22eed09ae9cde1c
Eric Boisvenu <burn4live@gmail.com>
2019-12-18 19:18:14 -0700

    Added addToPage

--- layouts/default.vue
+++ layouts/default.vue
@@ -103,7 +103,7 @@
           <v-btn v-if="this.$auth.loggedIn" color="amber darken-4" small @click="logout">
             Logout
           </v-btn>
-          <v-btn :style="{'display': addBtnShow}" class="addBtn">
+          <v-btn :style="{'display': addBtnShow}" class="addBtn" @click="addToPage">
             Add To Page
           </v-btn>
         </v-list-item>
@@ -299,7 +299,8 @@ export default {
     notifMessages: [],
     notifMeetings: [],
     notifBidRequest: [],
-    notifBids: []
+    notifBids: [],
+    deferredPrompt: ''
   }),
   computed: mapGetters({
     read: 'messages/Read',
@@ -343,35 +344,33 @@ export default {
         // this.$OneSignal.setEmail(this.$auth.user.email)
         // this.$OneSignal.sendTags({ key: this.$auth.user.sub })
       })
-      let deferredPrompt
       window.addEventListener('beforeinstallprompt', (e) => {
         // Prevent Chrome 67 and earlier from automatically showing the prompt
         e.preventDefault()
         // Stash the event so it can be triggered later.
-        deferredPrompt = e
+        this.deferredPrompt = e
         // Update UI to notify the user they can add to home screen
         this.addBtnShow = 'block'
-
-        addBtn.addEventListener('click', (e) => {
-          // hide our user interface that shows our A2HS button
-          this.addBtnShow = 'none'
-          // Show the prompt
-          deferredPrompt.prompt()
-          // Wait for the user to respond to the prompt
-          deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-              console.log('User accepted the A2HS prompt')
-            } else {
-              console.log('User dismissed the A2HS prompt')
-            }
-            deferredPrompt = null
-          })
-        })
       })
       this.picture = this.$auth.user.picture
     }
   },
   methods: {
+    addToPage () {
+      // hide our user interface that shows our A2HS button
+      this.addBtnShow = 'none'
+      // Show the prompt
+      this.deferredPrompt.prompt()
+      // Wait for the user to respond to the prompt
+      this.deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt')
+        } else {
+          console.log('User dismissed the A2HS prompt')
+        }
+        this.deferredPrompt = null
+      })
+    },
     login () {
       this.$auth.loginWith('auth0')
     },
