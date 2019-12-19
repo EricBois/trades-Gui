4da18a5a8ca11893f8de908146d9bea47960aeef
Eric Boisvenu <burn4live@gmail.com>
2019-12-18 19:56:41 -0700

    fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -103,8 +103,7 @@
           <v-btn v-if="this.$auth.loggedIn" color="amber darken-4" small @click="logout">
             Logout
           </v-btn>
-          <!-- <v-btn :style="{'display': addBtnShow}" class="addBtn" @click="addToPage"> -->
-          <v-btn class=" ml-3" @click="addToPage" small>
+          <v-btn :style="{'display': addBtnShow}" class="addBtn ml-3" @click="addToPage" small>
             <v-icon>mdi-tablet-cellphone</v-icon>&nbsp;
             Install App
           </v-btn>
