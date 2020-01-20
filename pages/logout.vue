a40633586588005421c589e758626b3935137930
burn <burn4live@gmail.com>
2020-01-20 03:52:41 -0700

    updated logout template

--- pages/logout.vue
+++ pages/logout.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1> You are logged out!</h1>
+  <v-layout wrap>
+    <v-flex xs12 text-center>
+      <v-card max-width="650" class="mx-auto pa-6">
+        <v-card max-width="600" min-height="100" class="mx-auto" elevation="15" shaped>
+          <h1>Successfully logged out !</h1>
+          <span class="sub">You have Successfully logged out, see you next time!</span>
+        </v-card>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
