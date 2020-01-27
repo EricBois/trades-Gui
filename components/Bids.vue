5329e99d38fa40b51b721d7b9b941124b36f611a
burn <burn4live@gmail.com>
2020-01-27 12:38:24 +0800

    basic card with breakpoint

--- components/Bids.vue
+++ components/Bids.vue
@@ -1,6 +1,28 @@
 <template>
   <v-container>
-    {{ bids }}
+    <v-card
+      v-for="bid in bids"
+      :key="bid._id"
+      color="grey darken-2"
+      class="mt-2 pa-1"
+      raised
+      shaped
+    >
+      <v-layout wrap>
+        <v-flex xs8 sm5 class="pl-4">
+          {{ bid.createdBy }}
+        </v-flex>
+        <v-flex v-if="$vuetify.breakpoint.smAndUp" sm4 text-center>
+          {{ bid.trade }}
+        </v-flex>
+        <v-flex xs4 sm3 text-center>
+          ${{ bid.price }}
+        </v-flex>
+      </v-layout>
+    </v-card>
+    <!-- <v-flex xs12 class="mt-12">
+      {{ bids }}
+    </v-flex> -->
   </v-container>
 </template>
 <script>
