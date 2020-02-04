2a6040b4194f7e5fc700581f3aa64cc93b78da9c
Eric Boisvenu <burn4live@gmail.com>
2020-02-04 16:12:47 -0700

    fixed dispatch

--- middleware/notifications.js
+++ middleware/notifications.js
@@ -1,10 +1,11 @@
 export default function ({ store, $auth }) {
   if ($auth.loggedIn) {
     store.dispatch('notifications/getNotifications')
+    store.dispatch('messages/getMessages')
     setInterval(
       function () {
-        store.dispatch('notifications/getNotifications')
         store.dispatch('messages/getMessages')
+        store.dispatch('notifications/getNotifications')
       },
       300000
     )
