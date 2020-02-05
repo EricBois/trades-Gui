cfc0711c248ee4e38d4a1cdbab190b401565bab9
Eric Boisvenu <burn4live@gmail.com>
2020-02-05 15:15:03 -0700

    updated style

--- pages/home.vue
+++ pages/home.vue
@@ -1,11 +1,112 @@
 <template>
   <v-layout wrap>
-    <v-flex xs12 text-center>
-      <v-card max-width="650" class="mx-auto pa-6">
-        <v-card max-width="600" min-height="100" class="mx-auto" elevation="15" shaped>
-          <h1>Sub Hub community</h1>
-          <span class="sub">Where contractors meet</span>
+    <!-- Messages -->
+    <v-flex xs12 sm5 offset-sm-1 text-center class="pa-2">
+      <v-card class="mx-auto pa-4">
+        <v-chip label>
+          <h2 class="sub">
+            Messages
+          </h2>
+        </v-chip>
+        <v-card
+          class="mx-auto"
+          elevation="15"
+          shaped
+          color="grey darken-2"
+        >
+          <v-flex xs12 class="ml-6 pa-2" text-center>
+            <span>You have 0 new messages !</span>
+          </v-flex>
         </v-card>
+        </v-chip>
+      </v-card>
+    </v-flex>
+    <!-- Metings -->
+    <v-flex xs12 sm5 text-center class="pa-2">
+      <v-card class="mx-auto pa-4">
+        <v-chip label>
+          <h2 class="sub">
+            Meetings
+          </h2>
+        </v-chip>
+        <v-card
+          class="mx-auto"
+          elevation="15"
+          shaped
+          color="grey darken-2"
+        >
+          <v-flex xs12 class="ml-6 pa-2" text-center>
+            <span>No meeting activity !</span>
+          </v-flex>
+        </v-card>
+        </v-chip>
+      </v-card>
+    </v-flex>
+    <v-flex xs12 class="my-6" />
+    <!-- job posting -->
+    <v-flex xs12 sm6 text-center class="pa-2">
+      <v-card max-width="750" class="mx-auto pa-4">
+        <v-chip label>
+          <h2 class="sub">
+            Recent job postings
+          </h2>
+        </v-chip>
+        <v-card
+          max-width="700"
+          class="mx-auto"
+          elevation="15"
+          shaped
+          color="grey darken-2"
+        >
+          <v-flex xs12 class="ml-6 pa-2" text-left>
+            <span>Job posting link and info</span>
+          </v-flex>
+        </v-card>
+        </v-chip>
+      </v-card>
+    </v-flex>
+    <!-- Latest news -->
+    <v-flex xs12 sm6 text-center class="pa-2">
+      <v-card max-width="750" class="mx-auto pa-4">
+        <v-chip label>
+          <h2 class="sub">
+            Your Latest News
+          </h2>
+        </v-chip>
+        <v-card
+          max-width="700"
+          class="mx-auto"
+          elevation="15"
+          shaped
+          color="grey darken-2"
+        >
+          <v-flex xs12 class="ml-6 pa-2" text-left>
+            <span>- News from api with link</span>
+          </v-flex>
+        </v-card>
+        </v-chip>
+      </v-card>
+    </v-flex>
+    <!-- Important Messages -->
+    <v-flex xs12 text-center class="pa-2">
+      <v-card max-width="750" class="mx-auto pa-4">
+        <v-chip label>
+          <h2 class="sub">
+            Important messages
+          </h2>
+        </v-chip>
+        <v-card
+          max-width="700"
+          class="mx-auto"
+          elevation="15"
+          shaped
+          color="grey darken-2"
+        >
+          <v-flex xs12 class="ml-6 pa-2" text-center>
+            <span>message goes here</span>
+          </v-flex>
+        </v-card>
+        </v-chip>
       </v-card>
     </v-flex>
   </v-layout>
