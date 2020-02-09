af3f088db77d46ada4b33fde0edab9a2730281a1
Eric Boisvenu <burn4live@gmail.com>
2020-02-09 16:35:24 -0700

    added snackbar

--- pages/management.vue
+++ pages/management.vue
@@ -1,6 +1,19 @@
 <template>
   <v-container>
     <v-layout wrap>
+      <v-snackbar
+        v-model="snackbar"
+        bottom
+        :color="snackbarColor"
+      >
+        {{ snackbarText }}
+        <v-icon v-if="snackbarColor.includes('red')">
+          mdi-alert-outline
+        </v-icon>
+        <v-icon v-else>
+          mdi-check-circle-outline
+        </v-icon>
+      </v-snackbar>
       <v-flex xs12 text-center>
         <v-card class="mx-auto" max-width="800">
           <v-data-table
@@ -29,11 +42,16 @@
               </v-toolbar>
             </template>
             <template v-slot:item.code="{ item }">
-              <v-chip label dark @click="copy(item.code)">
+              <v-chip label color="blue darken-3" @click="copy(item.code)">
                 <v-icon class="mr-2">
                   mdi-content-copy
                 </v-icon>
                 {{ item.code }}
+              </v-chip>
+            </template>
+            <template v-slot:item.user="{ item }">
+              <v-chip dark>
+                {{ item.user }}
               </v-chip>
             </template>
             <template v-slot:item.used="{ item }">
@@ -42,17 +60,51 @@
               </v-btn>
             </template>
             <template v-slot:item.action="{ item }">
-              <v-chip v-if="!item.used" color="blue-grey darken-2" label @click="used(item.code)">
+              <v-chip v-if="!item.used" color="blue-grey darken-2" label @click="makeSure(item.code)">
                 <v-icon class="mr-2">
-                  mdi-checkbox-marked-circle-outline
+                  mdi-cursor-default-click
                 </v-icon>
-                Use Code
+                Used Code
               </v-chip>
             </template>
           </v-data-table>
         </v-card>
       </v-flex>
     </v-layout>
+    <v-dialog
+      v-model="dialog"
+      max-width="400"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Please Confirm
+        </v-card-title>
+
+        <v-card-text>
+          Did you use or gave this code away ?
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+
+          <v-btn
+            color="orange darken-3"
+            text
+            @click="dialog = false"
+          >
+            No
+          </v-btn>
+
+          <v-btn
+            color="green darken-1"
+            text
+            @click="used(currentCode)"
+          >
+            Yes, i did!
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 <script>
@@ -60,10 +112,17 @@ import { mapGetters } from 'vuex'
 export default {
   data () {
     return {
+      dialog: false,
+      currentCode: '',
+      snackbar: false,
+      snackbarColor: 'green darken-3',
+      snackbarText: '',
+      alert: false,
+      errorMsg: '',
       codes: [],
       headers: [
         {
-          text: 'Code',
+          text: 'Codes',
           align: 'left',
           sortable: false,
           value: 'code'
@@ -85,9 +144,14 @@ export default {
     }
   },
   methods: {
-    used (code) {
-      this.$axios.$post('account/usedCode', { code }).then((res) => {
+    makeSure (code) {
+      this.dialog = true
+      this.currentCode = code
+    },
+    used () {
+      this.$axios.$post('account/usedCode', { code: this.currentCode }).then((res) => {
         this.getCodes()
+        this.dialog = false
       })
     },
     getCodes () {
@@ -103,8 +167,11 @@ export default {
     async copy (text) {
       try {
         await this.$copyText(text)
+        this.snackbarText = 'Successfully Copied!'
+        this.snackbar = true
       } catch (e) {
-        console.error(e)
+        this.snackbarText = `Something went wrong!`
+        this.snackbar = true
       }
     }
   }
