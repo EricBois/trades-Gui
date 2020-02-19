2312968aaad1e5643efbb4cbd31f279b0e3c1a97
Eric Boisvenu <burn4live@gmail.com>
2020-02-18 17:02:13 -0700

    added user info

--- pages/hiring.vue
+++ pages/hiring.vue
@@ -306,6 +306,16 @@
         <v-card>
           <v-flex xs12 class="mt-4">
             <v-card
+              v-if="contractors.length < 1"
+              outlined
+              max-width="800"
+              class="mx-auto mt-2 ma-2"
+            >
+              <v-flex class="ibm body-2" text-center>
+                No one is looking for work at the moment
+              </v-flex>
+            </v-card>
+            <v-card
               v-for="contractor in contractors"
               :key="contractor.id"
               outlined
@@ -313,7 +323,7 @@
               class="mx-auto mt-2 ma-2"
             >
               <v-layout wrap>
-                <v-flex xs6 class="pa-1">
+                <v-flex xs8 class="pa-1" @click.self="dialogUser = !dialogUser">
                   <v-chip small color="amber lighten-3" label outlined @click="showProfile(contractor.user)">
                     <v-icon small class="mr-1">
                       mdi-information-outline
@@ -321,16 +331,73 @@
                     {{ contractor.name }}
                   </v-chip>
                 </v-flex>
-                <v-flex xs6 class="pa-1">
-                  <v-chip v-for="skill in contractor.skills" :key="skill.id" small outlined label>
-                    {{ skill }}
-                  </v-chip>
+                <v-flex xs4 class="pa-1">
+                  <v-btn color="blue darken-3" small @click="showDetails(contractor)">
+                    Details
+                  </v-btn>
                 </v-flex>
               </v-layout>
             </v-card>
           </v-flex>
         </v-card>
         <v-divider />
+      </v-card>
+    </v-dialog>
+    <v-dialog v-model="dialogUser" max-width="600">
+      <v-card class="px-3">
+        <v-toolbar dark color="blue">
+          <v-spacer />
+          <v-toolbar-title class="body-1">
+            User details
+          </v-toolbar-title>
+          <v-btn icon dark @click="dialogUser = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-layout class="pa-2" wrap>
+          <v-flex xs12 text-center>
+            <v-chip large outlined label>
+              {{ currentUser.name }}
+            </v-chip>
+          </v-flex>
+          <v-flex xs12>
+            Expected wage: <v-chip class="ma-1" label small>
+              {{ currentUser.hourly }}
+            </v-chip>
+          </v-flex>
+          <v-flex xs12>
+            Looking for work in:
+            <v-chip v-for="loc in currentUser.location" :key="loc.id" small class="ma-1" label>
+              {{ loc }}
+            </v-chip>
+          </v-flex>
+          <v-flex xs12>
+            Employment type:
+            <v-chip v-for="loc in currentUser.employment" :key="loc.id" small class="ma-1" label>
+              {{ loc }}
+            </v-chip>
+          </v-flex>
+          <v-flex xs12>
+            Experience: &nbsp;
+            {{ currentUser.experience }}
+          </v-flex>
+          <v-flex xs12>
+            References: &nbsp;
+            {{ currentUser.reference }}
+          </v-flex>
+          <v-flex xs12>
+            Skills:
+            <v-chip v-for="loc in currentUser.skills" :key="loc.id" small class="ma-1" label>
+              {{ loc }}
+            </v-chip>
+          </v-flex>
+          <v-flex xs12>
+            Tickets:
+            <v-chip v-for="loc in currentUser.tickets" :key="loc.id" small class="ma-1" label>
+              {{ loc }}
+            </v-chip>
+          </v-flex>
+        </v-layout>
       </v-card>
     </v-dialog>
   </v-container>
@@ -364,6 +431,7 @@ export default {
       dialogProfile: false,
       dialogEdit: false,
       dialogLfw: false,
+      dialogUser: false,
       currentUser: {},
       jobs: [],
       create: false,
@@ -465,6 +533,10 @@ export default {
         this.contractors = res
       })
       this.dialogLfw = true
+    },
+    showDetails (contractor) {
+      this.currentUser = contractor
+      this.dialogUser = true
     }
   }
 }
