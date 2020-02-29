0a935b43879bc186e80fb43823146325085bbe65
Eric Boisvenu <burn4live@gmail.com>
2020-02-29 09:11:09 -0700

    fixed mistake

--- nuxt.config.js
+++ nuxt.config.js
@@ -142,8 +142,8 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: "http://localhost:3000/v1/"
-    // baseURL: "https://sub-hub.ca/api/"
+    // baseURL: "http://localhost:3000/v1/"
+    baseURL: "https://sub-hub.ca/api/"
   },
   router: {
     middleware: ['auth']
