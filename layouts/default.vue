61d18f8a78bf615f3246a422c9729eab51ef248d
Eric Boisvenu <burn4live@gmail.com>
2020-03-28 08:17:16 -0600

    intent fixed

--- layouts/default.vue
+++ layouts/default.vue
@@ -600,10 +600,10 @@ export default {
         // Update UI to notify the user they can add to home screen
         this.addBtnShow = 'block'
       })
-      this.reff = document.referrer
       this.picture = this.$auth.user.picture
-      if (document.referrer.includes('android-app://')) {
+      if (document.referrer.includes('android-app://com.ebapps.subhub')) {
         location.href = 'intent:#Intent;action=notificationUid;end'
+        this.reff = document.referrer
         
       }
     }
